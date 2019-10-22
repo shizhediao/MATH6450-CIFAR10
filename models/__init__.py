4858b8893a50bd47d37994b4c6a47c58e69a19f4
diaoshizhe <654745845@qq.com>
2019-10-22 20:05:36 +0800

    remove useless code

--- models/__init__.py
+++ models/__init__.py
@@ -1,16 +1,5 @@
 # -*-coding:utf-8-*-
-from .lenet import *
-from .alexnet import *
-from .vgg import *
-from .resnet import *
-from .preresnet import *
-from .senet import *
-from .resnext import *
 from .densenet import *
-from .shake_shake import *
-from .sknet import *
-from .genet import *
-from .cbam_resnext import *
 
 
 def get_model(config):
